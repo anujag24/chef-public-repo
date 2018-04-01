a121e473e23c2092e8020bfa8dadab1c8c40de23
APO <P2742612@kstleacprol00068.(none)>
2018-04-01 15:53:09 -0500

    Updated code to install Java 7 and download tomcat binary

--- install-apache-tomcat/recipes/default.rb
+++ install-apache-tomcat/recipes/default.rb
@@ -4,7 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 #
-
+yum_package 'Install Open JDK7' do
+  package_name 'java-1.7.0-openjdk-devel'
+end
 group 'atomcat'
 
 user 'atomcat' do
@@ -13,6 +15,14 @@ user 'atomcat' do
   home '/opt/atomcat'
   system true
   shell '/sbin/nologin'
+end
+
+bash 'download tomcat 8.5.29' do
+  user 'atomcat'
+  cwd '/tmp'
+  code <<-EOH
+  wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.29/bin/apache-tomcat-8.5.29.tar.gz
+  EOH
 end
 
 
